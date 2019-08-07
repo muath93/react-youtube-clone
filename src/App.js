7c50892a058e8ba7e68be77785190ae7827c27ea
muath93 <mu3athme@gmail.com>
2019-08-07 23:01:54 +0300

    building a structure for App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div>
-      <h1>YouTube Clone App</h1>
-    </div>
-  );
+import { Grid } from '@material-ui/core';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Grid container justify="center" spacing={2}>
+        <Grid item xs={12}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              {/* SEARCH BAR */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* Videio DETAILS */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* VEDIO LIST */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
 
 export default App;
