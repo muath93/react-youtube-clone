a054c209c9054a74ac6d165c3bb316bc1c63e78d
muath93 <mu3athme@gmail.com>
2019-08-18 13:37:58 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
   render() {
     const { selectedVideo, videos } = this.state;
     return (
-      <Grid container justify="center" spacing={2}>
+      <Grid container justify="center" spacing={2} style={{ width: '99%' }}>
         <Grid item xs={12}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
